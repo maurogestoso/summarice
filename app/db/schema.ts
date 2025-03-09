350370b1219d53521c0558c3438e8da7c734f6a3
Mauro Gestoso <maurogestoso@gmail.com>
2025-03-09 11:00:52 +0100

    Add createdAt column to recipe table

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,3 +1,4 @@
+import { sql } from "drizzle-orm";
 import { index, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export let recipe = sqliteTable(
@@ -10,6 +11,7 @@ export let recipe = sqliteTable(
       ingredients: string[];
       instructions: string[];
     }>(),
+    createdAt: text("created_at").notNull().default(sql`CURRENT_TIMESTAMP`),
   },
   (table) => [index("userId_idx").on(table.userId)]
 );
